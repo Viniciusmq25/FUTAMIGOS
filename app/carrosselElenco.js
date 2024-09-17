dd777a1f1822d76913d4e9cbf893e8ad704af0e1
Viniciusmq25 <viniciusquintian@gmail.com>
2024-09-17 18:49:57 -0300

    carrosseis arrumados e "prontos"

--- app/carrosselElenco.js
+++ app/carrosselElenco.js
@@ -1,29 +1,84 @@
-$(document).ready(function() {
-
+$(document).ready(function() { 
   var $slider = $('.elenco');
-    $slider.slick({
-      infinite: true,
-      centerMode: true,
-      slidesToShow: 5,
-      slidesToScroll: 1,
-      swipeToSlide: false,
-      dots: true,
-      draggable: false,
-      responsive: [
-        {
-          breakpoint: 768,
-          settings: {
-            centerMode: true,
-            slidesToShow: 3
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            centerMode: true,
-            slidesToShow: 3
-          }
+  $slider.slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    swipeToSlide: true,
+    dots: true,
+    centerMode: true, // Ativar centerMode
+    focusOnSelect: true,
+    touchThreshold: 100,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToScroll: 3,
+          slidesToShow: 3,
+          swipeToSlide: true,
+          centerMode: true, // Manter centerMode ativado para telas maiores
         }
-      ]
-    });
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToScroll: 1,
+          slidesToShow: 1,
+          swipeToSlide: true,
+          centerMode: false, // Desativar centerMode para telas menores
+          dots: false
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToScroll: 1,
+          slidesToShow: 1,
+          swipeToSlide: true,
+          centerMode: false, // Desativar centerMode para telas menores
+          dots: false
+        }
+      }
+    ]
   });
+});
+
+
+
+$(document).ready(function() {
+  $('.musicas-futamigos').slick({
+    infinite:false,
+    dots: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToScroll: 1,
+          slidesToShow: 1,
+          swipeToSlide: true,
+          centerMode: false,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToScroll: 1,
+          slidesToShow: 1,
+          swipeToSlide: true,
+          centerMode: false,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToScroll: 1,
+          slidesToShow: 1,
+          swipeToSlide: true,
+          centerMode: false,
+        }
+      }
+    ]
+  });
+})
--- app/carrosselMusicas.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  $('.musicas-futamigos').slick({
-    infinite:false,
-    dots: true,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-  });
-})
-  
